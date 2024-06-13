cd0141b531bd2f8d767c8604445f0c7bbaa93cc0
Oluwasegun Atolagbe <58482668+AtVict0r@users.noreply.github.com>
2024-06-13 14:20:29 +0000

    part #2

--- first_server/index.js
+++ first_server/index.js
@@ -1,7 +1,22 @@
 const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {    
+    // console.log(req);
+
+    const { url } = req;
+    console.log(url);
+
+    if(url === '/translations') {
+        const transitions = { 1: 'one', 2: 'two', 3: 'three' };
+
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(transitions));
+
+        // res.end('translations')
+        res.end();
+    }
+
     res.end('Welcome to Node!');
 });
 
